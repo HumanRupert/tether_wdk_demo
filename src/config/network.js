a8047300fbcdfcf8efc146f94cd964e8545a4012
Ali Akhtari <aaliiaakhtarii@gmail.com>
2025-05-04 15:39:51 +0330

    [init]

--- src/config/network.js
+++ src/config/network.js
@@ -11,7 +11,7 @@ export const RPC_URL = `https://sepolia.infura.io/v3/${INFURA_KEY}`;
 
 export const EvmAccountAbstractionConfig = {
     rpcUrl: RPC_URL,
-    bundlerUrl: "https://api.stackup.sh/v1/sepolia/YOUR_ID",
+    bundlerUrl: "https://api.stackup.sh/v1/sepolia/BUNDLER_ID",
     entryPointAddress: "0x0576a174D229E3cFA37253523E645A78A0C91B57",
   };
   
